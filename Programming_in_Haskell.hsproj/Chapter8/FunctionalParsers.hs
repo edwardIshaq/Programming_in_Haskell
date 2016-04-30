c6905c7569330791c76b359e47aa64dcc05f10ef
Edward Ishaq <edward@zuno.com>
2016-04-30 08:58:19 -0700

    Following the original parser combination article on : http://www.cs.nott.ac.uk/~pszgmh/monparsing.pdf

--- Programming_in_Haskell.hsproj/Chapter8/FunctionalParsers.hs
+++ Programming_in_Haskell.hsproj/Chapter8/FunctionalParsers.hs
@@ -1,4 +1,6 @@
 module Chapter8.FunctionalParsers where
+--see orignial
+--http://www.cs.nott.ac.uk/~pszgmh/monparsing.pdf
 
 import            Prelude  hiding (return, (>>=))
 import qualified  Control.Monad as M
@@ -9,12 +11,12 @@ type Parser a = String -> [(a, String)]
 -- BASIC Parsers
 
 -- return always succeeds with the provided value
-return :: a -> Parser a
-return v = \inp -> [(v,inp)]
+result :: a -> Parser a
+result v = \inp -> [(v,inp)]
 
 -- failure will always fail
-failure :: Parser a
-failure = \inp -> []
+zero :: Parser a
+zero = \inp -> []
 
 -- item: will the head of string and tail as remainder
 -- if inp is empty it will fail
@@ -33,28 +35,51 @@ parse p inp = p inp
 Sequencing
 ----------}
 
-(>>=) :: Parser a -> (a -> Parser b) -> Parser b
-p >>= f = \inp -> case parse p inp of 
-                    [] -> []
-                    [(v,out)] -> parse (f v) out
+seq :: Parser a -> Parser b -> Parser (a,b)
+seq pa pb = \inp -> [((a,b), inp'') | (a, inp')  <- pa inp 
+                                    , (b, inp'') <- pb inp']
+
+
+-- pdf's parse version 
+bind :: Parser a -> (a -> Parser b) -> Parser b 
+p `bind` f = \inp -> concat [f v inp' | (v,inp') <- p inp]
+
+
+-- the books versio nof bind
+-- uses the parse function 
+bind' :: Parser a -> (a -> Parser b) -> Parser b
+p `bind'` f = \inp -> case p inp of 
+                      []        -> []
+                      [(v,out)]  -> parse (f v) out
+
+(>>=) = bind
     
 
 --example parse 1st char , ignore 2nd, parser 3rd
-p1_3 :: String -> [((Char, Char), String)]
-p1_3 = do c1 <- item
-          item
-          c2 <- item
-          return (c1, c2)
-   
 p1_3' :: Parser (Char, Char)   
 p1_3' =   item >>= \c1 ->
           item >>= \_  ->
           item >>= \c2 ->
-          return (c1,c2)
+          result (c1,c2)
 
 
+plus :: Parser a -> Parser a -> Parser a 
+p `plus` q = \inp -> (p inp ++ q inp)
 
---instance Monad (String -> [(a, String)]) where
---  return v = \inp -> [(v,inp)]
-  
-  
+sat :: (Char -> Bool) -> Parser Char
+sat p = item `bind` \c ->
+          if p c then result c else zero
+
+
+char :: Char -> Parser Char
+char x = sat (\y -> y == x)
+
+digit :: Parser Char 
+digit = sat (\c -> '0' <= c && c <= '9')
+
+lower :: Parser Char
+lower = sat (\x -> 'a' <= x && x <= 'z')
+
+upper :: Parser Char
+upper = sat (\x -> 'A' <= x && x <= 'Z')
+
