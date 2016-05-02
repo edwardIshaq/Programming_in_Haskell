75cba6806357d69efb937dac7a2b371a7f9e833b
Edward Ishaq <edward@zuno.com>
2016-05-02 14:34:47 -0700

    WIP on functionalParses2

--- Programming_in_Haskell.hsproj/Chapter8/FunctionalParsers2.hs
+++ Programming_in_Haskell.hsproj/Chapter8/FunctionalParsers2.hs
@@ -1,33 +1,33 @@
-module Chapter8.FunctionalParsers where
+module Chapter8.FunctionalParsers2 where
 --see orignial
 --http://www.cs.nott.ac.uk/~pszgmh/monparsing.pdf
 
 import            Prelude  hiding (return, (>>=))
 import qualified  Control.Monad as M
+import            Data.Char
 
+newtype Parser a = P(String -> [(a, String)])
 
-type Parser a = String -> [(a, String)]
 -- BASIC Parsers
-
 -- return always succeeds with the provided value
 result :: a -> Parser a
-result v = \inp -> [(v,inp)]
+result v = P(\inp -> [(v,inp)])
 
 -- failure will always fail
 zero :: Parser a
-zero = \inp -> []
+zero = P(\inp -> [])
 
 -- item: will the head of string and tail as remainder
 -- if inp is empty it will fail
 item :: Parser Char 
-item = \inp -> case inp of
+item = P(\inp -> case inp of
                 []      -> []
-                (x:xs)  -> [(x,xs)]
+                (x:xs)  -> [(x,xs)])
 
 
 -- parse: parse the string using explicit application                
 parse :: Parser a -> String -> [(a,String)]
-parse p inp = p inp
+parse (P p) inp = p inp
 
 
 {---------
@@ -35,8 +35,8 @@ Sequencing
 ----------}
 
 seq :: Parser a -> Parser b -> Parser (a,b)
-seq pa pb = \inp -> [((a,b), inp'') | (a, inp')  <- pa inp 
-                                    , (b, inp'') <- pb inp']
+seq (P pa) (P pb) = P(\inp -> [((a,b), inp'') | (a, inp')  <- pa inp 
+                                              , (b, inp'') <- pb inp'])
 
 {---------
 BIND
@@ -44,17 +44,17 @@ BIND
 
 -- pdf's parse version 
 bind :: Parser a -> (a -> Parser b) -> Parser b 
-p `bind` f = \inp -> concat [f v inp' | (v,inp') <- p inp]
+(P p) `bind` f = P(\inp -> concat [parse (f v) inp' | (v,inp') <- p inp])
 
 
 -- the books versio nof bind
 -- uses the parse function 
 bind' :: Parser a -> (a -> Parser b) -> Parser b
-p `bind'` f = \inp -> case p inp of 
-                      []        -> []
-                      [(v,out)]  -> parse (f v) out
+(P p) `bind'` f = P(\inp -> case p inp of 
+                      []          -> []
+                      [(v,out)]   -> parse (f v) out)
 
-(>>=) = bind
+(>>=) = bind'
     
 
 --example parse 1st char , ignore 2nd, parser 3rd
@@ -90,7 +90,7 @@ choice combinator
 --------}
 
 plus :: Parser a -> Parser a -> Parser a 
-p `plus` q = \inp -> (p inp ++ q inp)
+p `plus` q = P(\inp -> (parse p inp ++ parse  q inp))
 
 letter :: Parser Char
 letter = lower `plus` upper
@@ -112,18 +112,21 @@ word' =  letter `bind`  \x  ->
          word   `bind`  \xs -> 
          result (x:xs)
 
+parseMap :: (a -> b) -> Parser a -> Parser b
+parseMap f (P pa) = P(\inp -> case pa inp of 
+                            []          -> []
+                            [(v,out)]   -> [(f v, out)])
 
-{----
-This defenition can only take us this far to becoming a Monad.
-In order to make Parser into a Monad it has to be a type constructor. 
-Currently the def of Parser is just a type alias
-`type Parser a = String -> [(a, String)]`
+instance Functor Parser where
+  fmap = parseMap
+  
 
-it will need to be changed to a newtype:
-`type Parser a = P(String -> [(a, String)]`)
+pureParser = result
 
-where `P` will be the constructor.
-as a `newtype` the `P` constructor will be erased on runtime
-see: http://stackoverflow.com/questions/5889696/difference-between-data-and-newtype-in-haskell#5889784
+(<*>) :: Parser (a -> b) -> Parser a -> Parser b 
+(P f) <*> pa  = fmap f pa
 
-----}
+--instance Monad Parser where
+--  return  = result
+--  (>>=)   = bind
+  
